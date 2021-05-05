1d4db12a1175ab82e8e22d9b74821c0e31793619
bagotaj <bagotaj@gmail.com>
2021-05-05 22:12:31 +0200

    WhatsApp clone: implementing components - conversation part is working

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Login from './Login';
+import Dashboard from './Dashboard';
+import useLocalStorage from '../hooks/useLocalStorage';
+import { ContactsProvider } from '../contexts/ContactsProvider';
+import { ConversationsProvider } from '../contexts/ConversationsProvider';
+
+function App() {
+  const [id, setId] = useLocalStorage('id');
+
+  const dashboard = (
+    <ContactsProvider>
+      <ConversationsProvider>
+        <Dashboard id={id} />
+      </ConversationsProvider>
+    </ContactsProvider>
+  );
+
+  return id ? dashboard : <Login onIdSubmit={setId} />;
+}
+
+export default App;
